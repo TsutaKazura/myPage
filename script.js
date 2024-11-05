fb56880dc5cc928c9a3d4f315e6a77c0ca7169ea
TsutaKazura <ryoryo256@icloud.com>
2024-11-05 18:24:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let message = [1,2,3];
 import list from './data.js'; 
         
 // HTMLの<p>タグに変数の値を表示
-document.getElementById("output").textContent = list;
+document.getElementById("output").textContent = message;
