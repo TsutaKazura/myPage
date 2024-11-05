42d9ebd9f1149174a1d7a02ed5b5cca434553f54
TsutaKazura <ryoryo256@icloud.com>
2024-11-05 18:28:35 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 let message = [1,2,3];
 
-// import list from './data.js'; 
+import list from './data.js'; 
         
 // HTMLの<p>タグに変数の値を表示
 document.getElementById("output").textContent = message;
